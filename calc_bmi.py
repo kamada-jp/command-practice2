8a0af6c9e44f42fac458023c8e56ee090273ec45
kamada tomoki <t.kamada.sys20@morijyobi.ac.jp>
2021-03-26 11:11:38 +0900

    新規作成

---
+++ calc_bmi.py
@@ -0,0 +1,4 @@
+def bmi(h,j):
+    h = h/100
+    bmi = j/h**h
+    return bmi
---
+++ main.py
@@ -0,0 +1,6 @@
+import calc_bmi
+
+h = int(input("身長(cm):"))
+j = int(input("体重(kg):"))
+bmi = calc_bmi.bmi(h,j)
+print(bmi)
